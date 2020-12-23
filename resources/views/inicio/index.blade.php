0d63acb2b84a31ccadc1ca013cd41695c402c5be
luicahleo <luisrolandocahuanaleon@gmail.com>
2020-12-23 01:43:42 +0100

    primer commit

---
+++ resources/views/inicio/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('styles')
+
+@endsection
+
+@section('content')
+<div class="card mb-3" style="max-width: 540px;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img src="/luis.JPG" class="card-img"  style="height: 10%" alt="imagen Luis">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Luis Rolando Cahuana Leon</h5>
+          <p class="card-text">Ingeniero Electronico y Telecomunicaciones</p>
+            <a href="https://lacasadelpienso.org/">Pincha aqui para ver un pequeño trabajo</a>
+
+
+
+            <p style="color:#FF0000";>Sitio en Reconstruccion!!!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
